9fca2529c640da3e0a2b7a19d5285c3de5767cad
Brian Warner <warner@lothar.com>
2013-01-17 14:04:49 -0800

    got Firebase working, with a fake "websocket" module that uses a page-worker

--- ff/lib/backend.js
+++ ff/lib/backend.js
@@ -1,37 +1,28 @@
 
-//var Firebase = require("./firebase").Firebase;
-//var db;
-var data = require("self").data;
-
-var worker;
+var Firebase = require("./firebase-jetpack").Firebase;
+var db;
 
 exports.start = function() {
     console.log("starting DB");
-    //db = new Firebase("https://warner.firebaseio.com/tabthing");
-    //db.on("value", function(ss) {
-    //    console.log("new fb data", ss.val());
-    //});
-    //console.log("DB connection established");
-
-    worker = require("page-worker").Page({
-        contentUrl: data.url("worker.html"),
-        contentScriptfile: data.url("addon-comms.js")
+    db = new Firebase("https://warner.firebaseio.com/tabthing");
+    db.on("value", function(ss) {
+        console.log("new fb data", ss.val());
     });
-    worker.on("from-content", function(val) {
-        console.log("from db", JSON.stringify(val));
-    });
-    worker.port.emit("to-content", "early data");
+    console.log("DB connection created");
 
 };
 
 exports.set = function(value) {
-    worker.port.emit("set", value);
+    console.log("calling db.set");
+    db.set(value);
 };
 
+var counter = 0;
 exports.poke = function() {
     console.log("poke");
+    counter += 1;
     require("./comms").sendToAll("poke", {pokey: "poked"});
-    exports.set("new value");
+    exports.set("new value: "+counter);
     console.log("poked");
 };
 
